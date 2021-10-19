6febd9bbfb76e3b0c5bc635b83d02060b4ba9769
shruthishruu05 <shruthi.m@perfios.com>
2021-10-19 21:44:33 +0530

    ADDED : To save contact in local storage

--- js/address_form.js
+++ js/address_form.js
@@ -1,31 +1,67 @@
-const fullName = document.querySelector('#name');
-const textError = document.querySelector('.text-error');
-fullName.addEventListener('input', function() {
-    let nameRegex = RegExp('^[A-Z][a-z]{2,}');
-    if (nameRegex.test(fullName.value))
-        textError.textContent = "";
-    else textError.textContent = "Name is Incorrect";
-    
-});
+let contactObj = {};
 
-   
-    const number = document.querySelector('#tel');
-    const numberError = document.querySelector('.phone-error');
-    number.addEventListener('input', function() {
-    let numberRegex = RegExp("^(\\+\\d{2}|\\d{2})?\\d{10}$");
-    if (numberRegex.test(number.value))
-        numberError.textContent = "";
-    else numberError.textContent = "Mobile number is Incorrect";
-});
-
-const address = document.querySelector("#address");
-  const addressError = document.querySelector(".address-error");
-  address.addEventListener("input", function() {
-    let addressRegex = RegExp('([A-Z a-z 0-9]{3,})+')
-    if(addressRegex.test(address.value))
-    addressError.textContent = "";
-    else addressError.textContent = "Invalid Address!"
-
+document.addEventListener("DOMContentLoaded", (event) => {
+  const name = document.getElementById("name");
+  const nameError = document.querySelector(".text-error");
+  name.addEventListener("input", () => {
+    try {
+      validateName(name.value);
+      nameError.textContent = "";
+    } catch (e) {
+      nameError.textContent = e;
+    }
   });
+
+  const phone = document.getElementById("tel");
+  const phoneError = document.querySelector(".phone-error");
+  phone.addEventListener("input", () => {
+    try {
+      validatePhone(phone.value);
+      phoneError.textContent = "";
+    } catch (e) {
+      phoneError.textContent = e;
+    }
+  });
+
+  const address = document.getElementById("address");
+  const addressError = document.querySelector(".address-error");
+  address.addEventListener("input", () => {
+    try {
+      validateAddress(address.value);
+      addressError.textContent = "";
+    } catch (e) {
+      addressError.textContent = e;
+    }
+  });
+});
+
+const validateName = (name) => {
+  let nameRegex = RegExp("^[A-Z][a-zA-Z]{2}[a-zA-Z\\s]*$");
+  if (!nameRegex.test(name)) throw "Name is incorrect";
+};
+
+const validateAddress = (address) => {
+  address += " ";
+  let addressRegex = RegExp("^(.{3,}\\s){2,}$");
+  if (!addressRegex.test(address)) throw "Address is incorrect";
+};
+
+const validatePhone = (phone) => {
+  let phoneRegex = RegExp("^(\\+\\d{2}|\\d{2})?\\d{10}$");
+  if (!phoneRegex.test(phone)) throw "Phone number is incorrect";
+};
+
+const submitForm = (e) => {
+  console.log("Form Submitted");
+};
+
+const resetForm = () => {
+  console.log("Form Reset");
+};
+
+const getInputValue = (selector) => {
+  let value = document.querySelector(selector).value;
+  return value;
+};
 
 
