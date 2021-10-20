5f9791e1ab0be891b535cb0a6dad7895c59992fe
shruthishruu05 <shruthi.m@perfios.com>
2021-10-20 21:53:53 +0530

    ADDED : server url

--- js/site_properties.js
+++ js/site_properties.js
@@ -1,4 +1,7 @@
 let site_properties = {
     home_page: "../Pages/home.html",
     add_contact_page: "../Pages/address_form.html",
+    use_local_storage: false,
+    server_url: "http://127.0.0.1:3000/AddressDB/",
   };
+  
